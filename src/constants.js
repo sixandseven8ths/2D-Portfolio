e90b53891fef98eee915af622a87bfcf5ea4f0b5
Jeff <sixandseven8ths@gmail.com>
2024-11-07 12:45:36 -0600

    added resume

--- src/constants.js
+++ src/constants.js
@@ -13,8 +13,8 @@ export const dialogueData = {
      <a href="https://www.youtube.com/@freecodecamp" target="_blank">FreeCodeCamp</a>, <a href="https://www.youtube.com/@davidbombal" target="_blank">David Bombal</a>,
     <a href="https://www.youtube.com/@JeremysITLab" target="_blank">Jeremy's IT Lab</a>  and <a href="https://www.youtube.com/@GamersNexus" target="_blank">Gamer's Nexus</a>!`,
     bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-    resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-    Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
+    resume: `This is my desk and on it is my resume. <a href="https://github.com/sixandseven8ths/Resume/blob/main/Jeff%20Herold%20Resume.pdf" target="_blank">Check it out?</a>
+    Contact me at jeff@jeffreyherold.com if you have any interesting job opportunities!`,
     projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
     Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
     Kaboom.js by watching this tutorial <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
